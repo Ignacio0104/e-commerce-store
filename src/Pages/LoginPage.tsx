3e446c42b8502148aac93515e3c100c527d09d2c
Ignacio Smirlian <ignacio.smirlian@globant.com>
2023-05-23 17:04:22 -0300

    login google and add doc to users OK

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,18 +1,34 @@
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 import LoginForm from "../components/Login/LoginForm";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase/firebaseConfig";
 
 const LoginPage = () => {
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
+  const db = getFirestore(app);
 
   const handleGoogleLogin = () => {
     provider.setCustomParameters({ prompt: "select_account" });
     signInWithPopup(auth, provider)
-      .then((result) => {
+      .then(async (result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential?.accessToken;
-        const user = result.user;
-        console.log(user);
+        const email = result.user.email;
+        const docRef = collection(db, "users");
+        const userQuery = query(docRef, where("email", "==", email));
+        const querySnapshot = await getDocs(userQuery);
+        if (querySnapshot.docs.length > 0) {
+          console.log("user already in system");
+        } else {
+          console.log("new user");
+        }
       })
       .catch((error) => {
         const errorCode = error.code;
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,7 +1,183 @@
-import React from "react";
+import { Formik, Form } from "formik";
+import { useState, useEffect } from "react";
+import * as yup from "yup";
+import CustomInput from "../pure/CustomInput";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  addDoc,
+  collection,
+  doc,
+  getFirestore,
+  setDoc,
+} from "firebase/firestore";
+import { app } from "../../firebase/firebaseConfig";
+
+type PasswordVisibleState = {
+  [key: string]: boolean;
+};
+
+const loginSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(2, "Name too short ")
+    .required("This field is required!"),
+  lastname: yup
+    .string()
+    .min(2, "Lastname too short ")
+    .required("This field is required!"),
+  email: yup.string().email().required(),
+  password: yup
+    .string()
+    .min(8, "Password is too short")
+    .matches(/^(?=.*[a-z])/, "Must contain one lower case letter")
+    .matches(/^(?=.*[A-Z])/, "Must contain one upper case letter")
+    .matches(/^(?=.*[0-9])/, "Must contain one number")
+    .matches(/^(?=.*[!@#\$%\^&\*])/, "Must contain one special character")
+    .required("This field is required!"),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref("password")], "Passwords must match"),
+});
 
 const Register = () => {
-  return <div>Register</div>;
+  const [registerRequest, setRegisterRequest] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+  const [passwordVisible, setPasswordVisible] = useState<PasswordVisibleState>({
+    passwordOne: true,
+    passwordTwo: true,
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (field: string, value: string) => {
+    setRegisterRequest({ ...registerRequest, [field]: value });
+  };
+
+  const handleSubmit = async () => {
+    const auth = getAuth();
+    const db = getFirestore(app);
+
+    createUserWithEmailAndPassword(
+      auth,
+      registerRequest.email,
+      registerRequest.password
+    )
+      .then(async (userCredential) => {
+        const collectionRef = collection(db, "users");
+        await addDoc(collectionRef, {
+          name: registerRequest.name,
+          favorites: [],
+          email: registerRequest.email,
+          creditCards: [],
+          history: [],
+          address: "",
+          profilPic: "",
+          uid: userCredential.user.uid,
+        });
+        setError("");
+      })
+      .catch((error) => {
+        if (error.message.includes("already-in-use")) {
+          setError("The Email Address entered already exists in the system.");
+        } else {
+          setError(error.message);
+        }
+      });
+  };
+
+  const togglePassword = (index: string) => {
+    setPasswordVisible((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  };
+  return (
+    <div className="login-container">
+      <div className="manual-login-container">
+        <Formik
+          initialValues={registerRequest}
+          onSubmit={handleSubmit}
+          validationSchema={loginSchema}
+        >
+          {(props) => (
+            <Form className="input-container">
+              <CustomInput
+                label="Name"
+                name="name"
+                type="text"
+                placeholder="Name..."
+                handleChange={handleChange}
+              ></CustomInput>
+              <CustomInput
+                label="Lastname"
+                name="lastname"
+                type="text"
+                placeholder="Lastname..."
+                handleChange={handleChange}
+              ></CustomInput>
+              <CustomInput
+                label="Email"
+                name="email"
+                type="text"
+                placeholder="Email..."
+                handleChange={handleChange}
+              ></CustomInput>
+              <div className="password-container">
+                <CustomInput
+                  label="Password"
+                  name="password"
+                  type={passwordVisible.passwordOne ? "password" : "text"}
+                  placeholder="Password..."
+                  handleChange={handleChange}
+                ></CustomInput>
+                <div
+                  className="eye-icon-container"
+                  onClick={() => togglePassword("passwordOne")}
+                >
+                  {passwordVisible.passwordOne ? (
+                    <VisibilityIcon />
+                  ) : (
+                    <VisibilityOffIcon />
+                  )}
+                </div>
+              </div>
+              <div className="password-container">
+                <CustomInput
+                  label="Password"
+                  name="passwordConfirmation"
+                  type={passwordVisible.passwordTwo ? "password" : "text"}
+                  placeholder="Confirm Password..."
+                  handleChange={handleChange}
+                ></CustomInput>
+                <div
+                  className="eye-icon-container"
+                  onClick={() => togglePassword("passwordTwo")}
+                >
+                  {passwordVisible.passwordTwo ? (
+                    <VisibilityIcon />
+                  ) : (
+                    <VisibilityOffIcon />
+                  )}
+                </div>
+              </div>
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
+        {error && (
+          <div className="form-error-container">
+            <h4>{error}</h4>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Register;
